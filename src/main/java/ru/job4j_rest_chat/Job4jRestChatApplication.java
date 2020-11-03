9f7d404b16031658eef6c5e03801f0e62e68d4f5
DenisViskov <DenisViskov16@gmail.com>
2020-11-03 10:41:40 +0300

    Initial commit

---
+++ src/main/java/ru/job4j_rest_chat/Job4jRestChatApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j_rest_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4jRestChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Job4jRestChatApplication.class, args);
+    }
+
+}
